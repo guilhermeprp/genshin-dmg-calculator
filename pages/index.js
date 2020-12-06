750ec5a76fe27f74fbe61af80f704704a2382a49
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2020-12-06 17:08:02 -0300

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import TopBar from "../src/components/navbar/navbar.jsx";
+import { Fragment } from "react";
+import NavBar from "../src/components/navbar/navbar.js";
+import GlobalStyle from "../src/globalStyles.js";
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <TopBar />
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <body>
+      <GlobalStyle />
+      <NavBar />
+    </body>
+  );
 }
 
 export default Home;
