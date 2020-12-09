7137595846a43d51e699bba143ff0960552d86a5
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2020-12-09 01:21:42 -0300

    correções estilização navbar

--- src/components/navbar/navbarStyles.js
+++ src/components/navbar/navbarStyles.js
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 
 export const Navbar = styled.nav`
+  z-index: 100;
   background-color: #0f111a;
   width: 100%;
-  height: 70px;
-  display: block;
+  position: fixed;
+  display: flex;
   text-align-last: center;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
 
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  li {
+  img {
     display: block;
     float: left;
-    margin: 20px 0px 20px 30vw;
+    /* margin: 20px 0px 20px 30vw; */
     color: #a6accd;
     font-size: 20px;
     font-weight: 500;
     letter-spacing: 0px;
+    height: 100%;
     :active {
       color: #c41e1e;
       transition: 0.3s ease;
@@ -29,54 +29,28 @@ export const Navbar = styled.nav`
     }
   }
 
-  img {
-    position: absolute;
-    top: 5px;
-    width: auto;
-    height: 60px;
-    right: 45vw;
-    left: 45vw;
-  }
-
   button {
     display: none;
   }
 
   @media screen and (max-width: 576px) {
-    img {
-      right: 35vw;
-      left: 35vw;
-    }
-    li {
-      margin: 20px 0px 20px 35px;
-      font-size: 20px;
-      font-weight: 500;
-    }
-
     button {
       display: block;
+      position: absolute;
       background-color: #474c5c;
       border-color: transparent;
       height: 100%;
       width: 70px;
       float: right;
+      right: 0;
+      outline: transparent;
     }
   }
   @media screen and (max-width: 375px) {
-    li {
-      margin: 25px 0px 25px 30px;
-      font-size: 17px;
-    }
+    z-index: 1;
   }
+
   @media screen and (max-width: 320px) {
-    height: 50px;
-    li {
-      margin: 1rem 0px 1rem 30px;
-      font-size: 14px;
-    }
-    img {
-      top: 0px;
-      height: 50px;
-    }
+    z-index: 1;
   }
 `;
