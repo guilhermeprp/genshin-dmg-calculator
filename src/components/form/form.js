556fad9751d9ebc5aabf7781edd45f7aaeab9752
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-01-03 09:20:17 -0300

    added img, removed unused events

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { ContainerCenter } from "./formStyles.js";
-import { inputBlur, inputFocus } from "./formEvents.js";
 
 function Form() {
   return (
     <ContainerCenter>
+      <img src="https://wallpaperaccess.com/full/2505746.jpg" />
       <form>
         <h1>Figure out your Damage!!</h1>
         <div>
           <div className="input-block">
-            <input
-              id="input-character-flat-atk"
-              type="text"
-              onBlur={inputBlur}
-              onFocus={inputFocus}
-            ></input>
+            <input type="text" id="input-character-flat-atk" />
             <label id="label-character-flat-atk" for="input-character-flat-atk">
               Character Flat Atk
             </label>
           </div>
           <div className="input-block">
-            <input id="input-character-passive-atk" type="text"></input>
+            <input type="text" id="input-character-passive-atk" />
             <label
               id="label-character-passive-atk"
               for="input-character-passive-atk"
@@ -29,13 +24,13 @@ function Form() {
             </label>
           </div>
           <div className="input-block">
-            <input id="input-weapon-flat-atk" type="text"></input>
+            <input type="text" id="input-weapon-flat-atk" />
             <label id="label-weapon-flat-atk" for="input-weapon-flat-atk">
               Weapon Flat Atk
             </label>
           </div>
           <div className="input-block">
-            <input id="input-weapon-percentage-atk" type="text"></input>
+            <input type="text" id="input-weapon-percentage-atk" />
             <label
               id="label-weapon-percentage-atk"
               for="input-weapon-percentage-atk"
@@ -44,13 +39,13 @@ function Form() {
             </label>
           </div>
           <div className="input-block">
-            <input id="input-artifact-flat-atk" type="text"></input>
+            <input type="text" id="input-artifact-flat-atk" />
             <label id="label-artifact-flat-atk" for="input-artifact-flat-atk">
               Artifact Flat Atk
             </label>
           </div>
           <div className="input-block">
-            <input id="input-artifact-percentage-atk" type="text"></input>
+            <input type="text" id="input-artifact-percentage-atk" />
             <label
               id="label-artifact-percentage-atk"
               for="input-artifact-percentage-atk"
@@ -59,13 +54,13 @@ function Form() {
             </label>
           </div>
           <div className="input-block">
-            <input id="input-character-flat-dmg" type="text"></input>
+            <input type="text" id="input-character-flat-dmg" />
             <label id="label-character-flat-dmg" for="input-character-flat-dmg">
               Elemental/Physical Dmg
             </label>
           </div>
           <div className="input-block">
-            <input id="input-critical-percentage-dmg" type="text"></input>
+            <input type="text" id="input-critical-percentage-dmg" />
             <label
               id="label-critical-percentage-dmg"
               for="input-critical-percentage-dmg"
@@ -74,7 +69,7 @@ function Form() {
             </label>
           </div>
           <div className="input-block">
-            <input id="input-skill-percentage-dmg" type="text"></input>
+            <input type="text" id="input-skill-percentage-dmg" />
             <label
               id="label-skill-percentage-dmg"
               for="input-skill-percentage-dmg"
@@ -83,19 +78,19 @@ function Form() {
             </label>
           </div>
           <div className="input-block">
-            <input id="input-character-level" type="text"></input>
+            <input type="text" id="input-character-level" />
             <label id="label-character-level" for="input-character-level">
               Character Level
             </label>
           </div>
           <div className="input-block">
-            <input id="input-enemy-level" type="text"></input>
+            <input type="text" id="input-enemy-level" />
             <label id="label-enemy-level" for="input-enemy-level">
               Enemy Level
             </label>
           </div>
           <div className="input-block">
-            <input id="input-enemy-resistance" type="text"></input>
+            <input type="text" id="input-enemy-resistance" />
             <label id="label-enemy-resistance" for="input-enemy-resistance">
               Enemy Resistance %
             </label>
