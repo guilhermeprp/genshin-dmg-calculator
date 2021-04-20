62e41ff522497cba9ce71f9d71871a7a51b40639
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-04-20 02:33:04 -0300

    finalizado calculo de dano

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -7,7 +7,17 @@ class Form extends React.Component {
 
     this.state = {
       charBaseAtk: null,
-      atkPercentage: null,
+      charPercentage: 0,
+      weaponBaseAtk: null,
+      weaponPercentage: 0,
+      artifactFlatAtk: 0,
+      artifactPercentage: 0,
+      bonusDmgPercentage: null,
+      criticalDmg: null,
+      skillMultiplier: null,
+      charLevel: null,
+      enemyLevel: null,
+      enemyRes: 0,
       total: null,
     };
   }
@@ -16,23 +26,85 @@ class Form extends React.Component {
     let name = event.target.name;
     let value = event.target.value;
     this.setState({ [name]: value });
-    // console.log(event.target.value);
+    console.log(event.target.value);
   };
 
   handleCalculate = () => {
-    // console.log(typeof charBaseAtk, charBaseAtk);
-
     console.log(typeof this.state.total, this.state.total);
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     let charBaseAtk = parseInt(this.state.charBaseAtk);
-    let atkPercentage = parseInt(this.state.atkPercentage);
-    this.state.total = charBaseAtk + atkPercentage;
+    let charPercentage = parseInt(this.state.charPercentage)
+      ? parseInt(this.state.charPercentage)
+      : 0;
+    let weaponBaseAtk = parseInt(this.state.weaponBaseAtk);
+    let weaponPercentage = parseInt(this.state.weaponPercentage)
+      ? parseInt(this.state.weaponPercentage)
+      : 0;
+    let artifactFlatAtk = parseInt(this.state.artifactFlatAtk);
+    let artifactPercentage = parseInt(this.state.artifactPercentage)
+      ? parseInt(this.state.artifactPercentage)
+      : 0;
+    let bonusDmgPercentage = parseInt(this.state.bonusDmgPercentage) / 100 + 1;
+    let criticalDmg = parseInt(this.state.criticalDmg) / 100 + 1;
+    let skillMultiplier = parseInt(this.state.skillMultiplier) / 100 + 1;
+    let charLevel = parseInt(this.state.charLevel) + 100;
+    let enemyLevel = parseInt(this.state.enemyLevel) + 100;
+    let enemyRes = parseInt(this.state.enemyRes)
+      ? parseInt(this.state.enemyRes)
+      : 0;
+
+    console.log(
+      typeof charBaseAtk,
+      charBaseAtk,
+      typeof charPercentage,
+      charPercentage,
+      typeof weaponBaseAtk,
+      weaponBaseAtk,
+      typeof weaponPercentage,
+      weaponPercentage,
+      typeof artifactFlatAtk,
+      artifactFlatAtk,
+      typeof artifactPercentage,
+      artifactPercentage,
+      typeof bonusDmgPercentage,
+      bonusDmgPercentage,
+      typeof criticalDmg,
+      criticalDmg,
+      typeof skillMultiplier,
+      skillMultiplier,
+      typeof charLevel,
+      charLevel,
+      typeof enemyLevel,
+      enemyLevel,
+      typeof enemyRes,
+      enemyRes
+    );
+
+    var baseAtk = charBaseAtk + weaponBaseAtk;
+    var atkPercentage =
+      1 + (charPercentage + weaponPercentage + artifactPercentage) / 100;
+    var levelDiffDef = charLevel / (charLevel + enemyLevel);
+    var totalAtk = baseAtk * atkPercentage + artifactFlatAtk;
+
+    console.log(typeof baseAtk, baseAtk);
+    console.log(typeof atkPercentage, atkPercentage);
+    console.log(typeof levelDiffDef, levelDiffDef);
+    console.log(typeof totalAtk, totalAtk);
+
+    console.log(totalAtk * bonusDmgPercentage * skillMultiplier);
+    console.log(
+      (totalAtk * bonusDmgPercentage * skillMultiplier * levelDiffDef) /
+        (enemyRes / 100 + 1)
+    );
+
+    this.state.total = parseInt(
+      (totalAtk * bonusDmgPercentage * skillMultiplier * levelDiffDef) /
+        (enemyRes / 100 + 1)
+    );
     this.setState({ total: this.state.total });
-    // let charBaseAtk = this.state.charBaseAtk;
-    // let atkPercentage = this.state.atkPercentage;
   };
 
   render() {
@@ -52,17 +124,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
+                  id="input-character-flat-atk"
                   className="input-text"
                   name="charBaseAtk"
-                  id="input-character-flat-atk"
-                  value={this.state.value}
-                  onChange={this.handleChange}
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-character-flat-atk"
                   htmlFor="input-character-flat-atk"
                 >
-                  Character Flat Atk
+                  Character Flat Atk <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -70,10 +141,10 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
-                  name="atkPercentage"
                   id="input-character-passive-atk"
-                  onChange={this.handleChange}
+                  className="input-text"
+                  name="charPercentage"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-character-passive-atk"
@@ -87,14 +158,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-weapon-flat-atk"
+                  className="input-text"
+                  name="weaponBaseAtk"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-weapon-flat-atk"
                   htmlFor="input-weapon-flat-atk"
                 >
-                  Weapon Flat Atk
+                  Weapon Flat Atk <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -102,8 +175,10 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-weapon-percentage-atk"
+                  className="input-text"
+                  name="weaponPercentage"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-weapon-percentage-atk"
@@ -117,14 +192,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-artifact-flat-atk"
+                  className="input-text"
+                  name="artifactFlatAtk"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-artifact-flat-atk"
                   htmlFor="input-artifact-flat-atk"
                 >
-                  Artifact Flat Atk
+                  Artifact Flat Atk <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -132,8 +209,10 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-artifact-percentage-atk"
+                  className="input-text"
+                  name="artifactPercentage"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-artifact-percentage-atk"
@@ -147,14 +226,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-character-flat-dmg"
+                  className="input-text"
+                  name="bonusDmgPercentage"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-character-flat-dmg"
                   htmlFor="input-character-flat-dmg"
                 >
-                  Elemental/Physical Dmg
+                  Elemental/Physical Dmg <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -162,14 +243,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-critical-percentage-dmg"
+                  className="input-text"
+                  name="criticalDmg"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-critical-percentage-dmg"
                   htmlFor="input-critical-percentage-dmg"
                 >
-                  Critical Dmg %
+                  Critical Dmg % <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -177,14 +260,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-skill-percentage-dmg"
+                  className="input-text"
+                  name="skillMultiplier"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-skill-percentage-dmg"
                   htmlFor="input-skill-percentage-dmg"
                 >
-                  Skill Multiplier %
+                  Skill Multiplier % <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -192,14 +277,16 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-character-level"
+                  className="input-text"
+                  name="charLevel"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-character-level"
                   htmlFor="input-character-level"
                 >
-                  Character Level
+                  Character Level <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -207,11 +294,13 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-enemy-level"
+                  className="input-text"
+                  name="enemyLevel"
+                  onBlur={this.handleChange}
                 />
                 <label id="label-enemy-level" htmlFor="input-enemy-level">
-                  Enemy Level
+                  Enemy Level <span className="obrigatorio">*</span>
                 </label>
               </div>
             </div>
@@ -219,8 +308,10 @@ class Form extends React.Component {
               <div className="input-block">
                 <input
                   type="text"
-                  className="input-text"
                   id="input-enemy-resistance"
+                  className="input-text"
+                  name="enemyRes"
+                  onBlur={this.handleChange}
                 />
                 <label
                   id="label-enemy-resistance"
