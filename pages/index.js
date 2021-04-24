e630975210273ad543e0a919b9e31150d72157db
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-04-24 04:24:51 -0300

    cypress aded to unit tests

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
-import { Fragment } from "react";
+import Head from "next/head";
+import React from "react";
 import GlobalStyle from "../src/globalStyles";
 import NavBar from "../src/components/navbar/navbar.jsx";
 import Form from "../src/components/form/form.jsx";
 
 function Home() {
   return (
-    <body>
-      <GlobalStyle />
-      <NavBar />
-      <Form />
-    </body>
+    <html>
+      <Head>
+        <title>Genshin DMG</title>
+        <link rel="icon" href="../public/favicon.ico" />
+      </Head>
+      <body>
+        <GlobalStyle />
+        <NavBar />
+        <Form />
+      </body>
+    </html>
   );
 }
 
